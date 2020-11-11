5826f97f4bca240af41e806fb3191cafdc0cd3c0
Andrew Wallace <andrew.wallace.english@gmail.com>
2020-11-11 12:28:39 +0900

    what?

---
+++ roomlist.js
@@ -0,0 +1,42 @@
+const rooms2={
+	mainRoom:{
+		roomName: "roomTest",
+		roomText: "You enter the room. Torchlight flickers down the corridor ahead.",
+		option1:{
+			optionName: "newRoom",
+			optionText:  "Go North"
+		},
+		option2:{
+			optionName: null,
+			optionText: "Go West"
+		},
+		option3:{
+			optionName: null,
+			optionText: "Go South"
+		},
+		option4:{
+			optionName: null,
+			optionText: "Go East"
+		}
+	},
+	newRoom:{
+		roomName: "newRoom",
+		roomText: "You awake to the pain of a new existence in a dank womb of darkness and decay.",
+		option1:{
+			optionName: null,
+			optionText: "Go North"
+		},
+		option2:{
+			optionName: null,
+			optionText: "Go West"
+		},
+		option3:{
+			optionName: null,
+			optionText: "Go South"
+		},
+		option4:{
+			optionName: "mainRoom",
+			optionText: "Go North"
+		}
+	}
+}
